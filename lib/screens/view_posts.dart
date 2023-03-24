9988e2321290684dc23f5c22e1c4a99e9801bee9
Harsh <anandharsh199@gmail.com>
2023-03-24 15:22:50 +0530

    Going to Merge

--- lib/screens/view_posts.dart
+++ lib/screens/view_posts.dart
@@ -1,8 +1,8 @@
 import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
-import 'package:awesome_dialog/awesome_dialog.dart';
-import'package:firebase_storage/firebase_storage.dart' as firebase_storage;
 
+import'package:firebase_storage/firebase_storage.dart' as firebase_storage;
+import 'package:awesome_dialog/awesome_dialog.dart';
 
 class ViewPosts extends StatefulWidget {
   final String title;

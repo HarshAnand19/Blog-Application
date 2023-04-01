1e86e107424328113c0150118ffbc621537c9c47
Harsh <anandharsh199@gmail.com>
2023-04-01 12:04:06 +0530

    MultiProviders added

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:blog_app/provider/theme_provider.dart';
+import 'package:blog_app/provider/user_provider.dart';
 import 'package:blog_app/screens/option_screen.dart';
 import 'package:blog_app/screens/splash_screen.dart';
 import 'package:firebase_core/firebase_core.dart';
@@ -19,41 +20,24 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
-  Widget build(BuildContext context) =>
-
-     ChangeNotifierProvider(
-       create: (context)=> ThemeProvider(),
-       builder: (context,_) {
-         final themeProvider = Provider.of<ThemeProvider>(context);
-         return MaterialApp(
-           debugShowCheckedModeBanner: false,
-           title: 'Blog Demo',
-           themeMode:themeProvider.themeMode,
-           theme: MyThemes.lightTheme,
-           darkTheme: MyThemes.darkTheme,
-           home: SplashScreen(),
-         );
-
-       },
-     );
-
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key});
-
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-
-
-  @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.white,
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (context) => ThemeProvider()),
+        ChangeNotifierProvider(create: (_)=> UserProvider()),
+
+      ],
+      builder: (context, child) {
+        final themeProvider = Provider.of<ThemeProvider>(context);
+        return MaterialApp(
+          debugShowCheckedModeBanner: false,
+          title: 'Blog Demo',
+          themeMode: themeProvider.themeMode,
+          theme: MyThemes.lightTheme,
+          darkTheme: MyThemes.darkTheme,
+          home: SplashScreen(),
+        );
+      },
     );
   }
 }
--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -1,11 +1,13 @@
 import 'dart:async';
 
 import 'package:blog_app/main.dart';
+import 'package:blog_app/provider/user_provider.dart';
 import 'package:blog_app/screens/home_screen.dart';
 import 'package:blog_app/screens/login_screen.dart';
 import 'package:blog_app/screens/option_screen.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({Key? key}) : super(key: key);
@@ -27,6 +29,7 @@ class _SplashScreenState extends State<SplashScreen> {
 
     final user=auth.currentUser;
     if(user!=null){
+      addData();
       Timer(Duration(seconds: 3),() => Navigator.pushReplacement
         (context, MaterialPageRoute(builder: (context)=>HomeScreen(name:user.displayName.toString()))));
     }else{
@@ -35,6 +38,10 @@ class _SplashScreenState extends State<SplashScreen> {
     }
   }
 
+  addData() async{
+    UserProvider _userProvider=Provider.of<UserProvider>(context,listen: false);
+    await _userProvider.refreshUser();
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(

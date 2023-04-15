9e2285692638dc0379d113cc39f4159c8c4f59a9
Harsh <anandharsh199@gmail.com>
2023-04-15 16:11:32 +0530

    Edit Posts feature added

--- lib/screens/view_posts.dart
+++ lib/screens/view_posts.dart
@@ -1,24 +1,24 @@
 import 'dart:io';
-
 import 'package:awesome_dialog/awesome_dialog.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:firebase_database/firebase_database.dart';
+
 import 'package:flutter/material.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:share_plus/share_plus.dart';
-import'package:http/http.dart' as http;
+import 'package:http/http.dart' as http;
 
-class ViewPosts extends StatelessWidget {
-  final String title;
-  final String desc;
+
+class ViewPosts extends StatefulWidget {
+  late  String title;
+  late  String desc;
   final String photo;
   final String id;
   final String date;
   final String time;
   final String profImage;
 final String postId;
-  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+
   ViewPosts({
     Key? key,
     required this.title,
@@ -32,7 +32,24 @@ final String postId;
   }) : super(key: key);
 
   @override
+  State<ViewPosts> createState() => _ViewPostsState();
+}
+
+class _ViewPostsState extends State<ViewPosts> {
+  late String _updatedTitle;
+  late String _updatedDesc;
+
+  @override
+  void initState() {
+    super.initState();
+    _updatedTitle = widget.title;
+    _updatedDesc = widget.desc;
+  }
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+
+  @override
   Widget build(BuildContext context) {
+
 FirebaseAuth _auth =FirebaseAuth.instance;
 
     return Scaffold(
@@ -40,22 +57,32 @@ FirebaseAuth _auth =FirebaseAuth.instance;
         title: Row(
           children: [
             Expanded(
-              child: Text(title ?? ''),
+              child: Text(widget.title ?? ''),
             ),
             IconButton(
               onPressed: () {
-            _sharefiles();
+                _sharefiles();
               },
               icon: Icon(Icons.share),
             ),
 
-            if(id == _auth.currentUser!.uid)
+            if(widget.id == _auth.currentUser!.uid)
+              IconButton(
+                onPressed: () {
+                  _showEditDialog(context);
+                },
+                icon: Icon(Icons.edit),
+              ),
+            
+            if(widget.id == _auth.currentUser!.uid)
             IconButton(
               onPressed: () {
-            _showDialog(context);
+                _showDialog(context);
               },
               icon: Icon(Icons.delete),
             ),
+
+
           ],
         ),
         centerTitle: true,
@@ -68,7 +95,7 @@ FirebaseAuth _auth =FirebaseAuth.instance;
           children: [
             Image.network(
               width: double.infinity,
-              photo,
+              widget.photo,
               height: MediaQuery.of(context).size.height*.4,
             ),
             Padding(
@@ -77,28 +104,28 @@ FirebaseAuth _auth =FirebaseAuth.instance;
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
                   Text(
-                    title,
+                    widget.title,
                     style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
                   ),
                   SizedBox(height: 8),
                   Row(
                     children: [
                       CircleAvatar(
-                        backgroundImage: NetworkImage(profImage),
+                        backgroundImage: NetworkImage(widget.profImage),
                         radius: 16,
                       ),
                       SizedBox(width: 8),
                       Column(
                         crossAxisAlignment: CrossAxisAlignment.start,
                         children: [
-                          Text(date),
-                          Text(time),
+                          Text(widget.date),
+                          Text(widget.time),
                         ],
                       ),
                     ],
                   ),
                   SizedBox(height: 8),
-                  Text(desc),
+                  Text(widget.desc),
                 ],
               ),
             ),
@@ -106,17 +133,6 @@ FirebaseAuth _auth =FirebaseAuth.instance;
         ),
       ),
     );
-  }
-
-  void _sharefiles() async{
-    String doc ="Hey! Checkout this blog, \n Title - ${title}\n Description - ${desc} ";
-    http.Response response = await http.get(Uri.parse(photo));
-    final directory = await getTemporaryDirectory();
-    final path = directory.path;
-    final file = File('$path/image.png');
-    file.writeAsBytes(response.bodyBytes);
-    Share.shareFiles(['$path/image.png'],text: doc,);
-
   }
 
   _showDialog(context){
@@ -130,9 +146,113 @@ FirebaseAuth _auth =FirebaseAuth.instance;
         btnCancelOnPress: () {
         },
         btnOkOnPress: () {
-          _firestore.collection('posts').doc(postId).delete();
+          _firestore.collection('posts').doc(widget.postId).delete();
           Navigator.pop(context);
         }
     )..show();
   }
+
+  void _sharefiles() async{
+    String doc ="Hey! Checkout this blog, \n Title - ${widget.title}\n Description - ${widget.desc} ";
+    http.Response response = await http.get(Uri.parse(widget.photo));
+    final directory = await getTemporaryDirectory();
+    final path = directory.path;
+    final file = File('$path/image.png');
+    file.writeAsBytes(response.bodyBytes);
+    Share.shareFiles(['$path/image.png'],text: doc,);
+
+  }
+
+  void _showEditDialog(BuildContext context) {
+    String updatedTitle = widget.title;
+    String updatedDesc = widget.desc;
+
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          icon: Icon(Icons.edit,color: Colors.blue,),
+          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20.0)),
+          elevation: 5.0,
+          title: Text("Edit Your  Post"),
+          content: SingleChildScrollView(
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                TextFormField(
+                  initialValue:widget.title,
+                  onChanged: (value) => updatedTitle = value,
+                  decoration: InputDecoration(
+                    labelText: 'Title',
+                    labelStyle: TextStyle(color: Colors.blue),
+                    hintText: "Enter updated Title",
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(20.0)
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(20.0),
+                      borderSide: BorderSide(
+                        width: 1.8,
+                        color: Colors.blue,
+                      ),
+                    ),
+
+                  ),
+                ),
+
+                SizedBox(height: MediaQuery.of(context).size.height*.02,),
+
+                TextFormField(
+                  initialValue: widget.desc,
+                  maxLines: null,
+                  onChanged: (value) => updatedDesc = value,
+                  decoration: InputDecoration(
+                      labelText: 'Description',
+                    labelStyle: TextStyle(color: Colors.blue),
+                      hintText: "Enter updated Description",
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(20.0)
+                      ),
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(20.0),
+                      borderSide: BorderSide(
+                        width: 1.8,
+                        color: Colors.blue,
+                      ),
+                  ),
+
+                ),
+                )
+              ],
+            ),
+          ),
+          actions: <Widget>[
+            TextButton(
+              child: Text("Cancel",style: TextStyle(color: Colors.red),),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+            TextButton(
+              child: Text("Save",style: TextStyle(color: Colors.blue),),
+              onPressed: () {
+                _firestore.collection('posts').doc(widget.postId).update({
+                  'title': updatedTitle,
+                  'description': updatedDesc,
+                }).then((value) {
+                  setState(() {
+                    // Rebuild the widget with the updated values
+                    widget.title = updatedTitle;
+                    widget.desc = updatedDesc;
+                  });
+                });
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
 }
